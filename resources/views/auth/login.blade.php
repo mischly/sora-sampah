c963bc7a2d694a44af846f48b22c4b0a89dac3b6
Aad Abdurahman <kats.misch@gmail.com>
2025-06-26 14:02:42 +0700

    Sedikit memperbaiki input field saat login error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,30 +8,46 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card-login card border-0 shadow-lg rounded-4 p-4 py-5   " style="background-color: #f8f9fa;">
+                <div class="card-login card border-0 shadow-lg rounded-4 p-4 py-5" style="background-color: #f8f9fa;">
                     <div class="row align-items-center">
                         <!-- Form Kiri -->
                         <div class="col-md-6 px-4">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
-                                <div class="mb-4 input-icon">
-                                    <i class="bi bi-person-fill icon-input"></i>
-                                    <input id="email" type="email" class="form-control rounded-pill ps-5 @error('email') is-invalid @enderror" name="email" placeholder="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                {{-- Email Field --}}
+                                <div class="mb-4">
+                                    <div class="input-wrapper @error('email') error @enderror">
+                                        <i class="bi bi-person-fill input-icon"></i>
+                                        <input id="email" 
+                                            type="email" 
+                                            class="form-control" 
+                                            name="email" 
+                                            placeholder="Email" 
+                                            value="{{ old('email') }}" 
+                                            required 
+                                            autocomplete="email" 
+                                            autofocus>
+                                    </div>
                                     @error('email')
-                                        <div class="invalid-feedback d-block">
-                                            <strong>{{ $message }}</strong>
-                                        </div>
+                                        <span class="error-message">{{ $message }}</span>
                                     @enderror
                                 </div>
 
-                                <div class="mb-4 input-icon">
-                                    <i class="bi bi-key-fill icon-input"></i>
-                                    <input id="password" type="password" class="form-control rounded-pill ps-5 @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
+                                {{-- Password Field --}}
+                                <div class="mb-4">
+                                    <div class="input-wrapper @error('password') error @enderror">
+                                        <i class="bi bi-key-fill input-icon"></i>
+                                        <input id="password" 
+                                            type="password" 
+                                            class="form-control" 
+                                            name="password" 
+                                            placeholder="Password" 
+                                            required 
+                                            autocomplete="current-password">
+                                    </div>
                                     @error('password')
-                                        <div class="invalid-feedback d-block">
-                                            <strong>{{ $message }}</strong>
-                                        </div>
+                                        <span class="error-message">{{ $message }}</span>
                                     @enderror
                                 </div>
 
@@ -51,15 +67,14 @@
 
                                 <div class="d-flex justify-content-center gap-3 social-login">
                                     <a href="#" class="social-icon">
-                                        <img src="{{ asset('images/google-icon.svg')    }}" alt="Facebook">
+                                        <img src="{{ asset('images/google-icon.svg') }}" alt="Google">
                                     </a>
                                     <a href="#" class="social-icon">
-                                        <img src="{{ asset('images/fb-icon.svg') }}" alt="Google">
+                                        <img src="{{ asset('images/fb-icon.svg') }}" alt="Facebook">
                                     </a>
                                 </div>
                             </form>
                         </div>
-
                         <!-- Gambar Kanan -->
                         <div class="col-md-6 d-none d-md-block text-center">
                             <img src="{{ asset('images/login-icon.png') }}" alt="Login Illustration" class="img-fluid" style="max-height: 280px;">
